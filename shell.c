0a521e10ae0b21b4c89a9418a8deaaea5d8d8c47
Rosalia Steffi Longjam <rosaliaslwork@gmail.com>
2022-10-25 19:19:21 +0530

    Updated shell.c

--- shell.c
+++ shell.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+char origin[1024];
 
 void initializeShell() {
     printf("\n\t################################################\n");
@@ -48,17 +51,59 @@ int echo(int argc, char* argv[]) {
     if (argc == 1) {
         printf("\n");
     } else {
-        for (i = 1; i < argc; i++) {
-            printf("%s ", argv[i]);
+        if(strcmp(argv[1], "-n") == 0){ //option to print the given statement without any \n for the next terminal prompt
+            for(i = 2; i < argc-1; i++){
+                printf("%s ", argv[i]);
+            }
+            printf("%s", argv[i]);
         }
-        printf("\n");
+        else if(strcmp(argv[1], "--help") == 0){ //displays the help page
+            char cwd[1024];
+	        getcwd(cwd, sizeof(cwd));
+            chdir("/");
+            chdir(origin);
+            chdir("Descriptions");
+            FILE *fp;
+            fp = fopen("echo.txt", "r");
+            char *fline = NULL;
+            size_t flen = 0;
+            while(getline(&fline, &flen, fp) != -1){
+                printf("%s", fline);
+            }
+            printf("\n");
+            fclose(fp);
+            chdir("/");
+            chdir(cwd);
+        }
+        else{
+            for(i = 1; i < argc; i++){ //no option implemented, it will print wrong/invalid options as well (error case handled)
+                printf("%s ", argv[i]);
+            }
+            printf("\n");
+        }
     }
     return 1;
 }
 
 int pwd(int argc, char* argv[]) {
-    char presentWorkingDirectory[1000];
-    printf("Current Working Directory: %s \n", getcwd(presentWorkingDirectory, sizeof(presentWorkingDirectory)));
+    if (argc == 1) {
+        printf("Current Working Directory: %s \n", getenv("PWD"));
+    }
+    else if(argc == 2) {
+        if (strcmp(argv[1], "-L") == 0) {
+            printf("Current Working Directory: %s \n", getenv("PWD"));
+        }
+        else if(strcmp(argv[1], "-P") == 0){
+            char presentWorkingDirectory[1000];
+            printf("Current Working Directory: %s \n", getcwd(presentWorkingDirectory, sizeof(presentWorkingDirectory)));
+        }
+        else{
+            printf("Invalid option\n");
+        }
+    }
+    else{
+        printf("Invalid option");
+    }
     return 1;
 }
 
@@ -94,10 +139,28 @@ int checkAndExecuteInternal(int argc, char** command) {
     return 0;
 }
 
+void checkAndExecuteExternal(int argc, char** command) {
+    if (strcmp(command[0], "cat") == 0) {
+
+	} else if (strcmp(command[0], "date") == 0) {
+
+	} else if (strcmp(command[0], "ls") == 0) {
+
+	} else if (strcmp(command[0], "mkdir") == 0) {
+
+	} else if (strcmp(command[0], "rm") == 0) {
+
+	} else {
+        printf("command not found: %s\n", command[0]);
+    }
+    return;
+}
+
 int main() {
     initializeShell();
     int shouldExit = 0;
-    int isInternalCommand = 0, isExternalCommand = 0;
+    int isInternalCommand = 0;
+    getcwd(origin, sizeof(origin));
 
     while (!shouldExit) {
         printCurrentDirectory();
@@ -117,6 +180,22 @@ int main() {
             }
             // printf("%d\n", argc);
             isInternalCommand = checkAndExecuteInternal(argc, command);
+            if (!isInternalCommand) {
+                pid_t pid = fork();
+                if (pid < 0) {
+				    printf("Error in creating child process.\n");
+                } else if (pid == 0) {
+                    printf("Created child process.\n");
+                    // printf("%s\n", command[0]);
+                    checkAndExecuteExternal(argc, command);
+                } else if (pid > 0) {
+                    pid_t p_temp = waitpid(pid, NULL, WUNTRACED);
+				    if(p_temp < 0) {
+					    printf("Error no %d\n", errno);
+					    perror("waitpid");
+				    }
+                }
+            }
         }
     }
 
